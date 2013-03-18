e295b649e3cc1bb4c0101aa600320f69dd96e91c
Hack Reactor Students <accounts@hackreactor.com>
2013-03-17 17:44:38 -0700

    Adds HashTable

--- spec/hashTableSpec.js
+++ spec/hashTableSpec.js
@@ -10,5 +10,47 @@ describe("hashTable", function() {
     expect(hashTable.retrieve).toEqual(jasmine.any(Function));
   });
 
-  // add more tests here to test the functionality of hashTable
+  it('should retrieve value after inserting an item', function() {
+    var some_string = "jonathan";
+    hashTable.insert(some_string);
+    expect(hashTable.retrieve(some_string)).toEqual(some_string);
+  });
+
+
+  it('should delete "jonathan" from storage', function() {
+    var some_string = "jonathan";
+    hashTable.insert(some_string);
+    some_string = "marilyn";
+    hashTable.insert(some_string);
+    hashTable.remove("jonathan");
+    expect(function(){ 
+      hashTable.retrieve("jonathan");
+    }).toThrow(new Error("Cannot retrieve string."));        
+//    expect(hashTable.retrieve("jonathan")).toEqual(null);        
+  })
+
+  it('should throw error retrieving a non-existant entry', function() {
+    expect(function() {
+      hashTable.retrieve("bugs bunny");
+    }).toThrow(new Error("Cannot retrieve string."));
+  })
+
+  it('should throw error if overriding an entry', function() {
+    var some_string = "jonathan"; //key evaluates to 5
+    hashTable.insert(some_string);
+    expect(function(){ 
+      debugger;    
+      hashTable.insert("sally"); // key evaluates to 5
+    }).toThrow(new Error("Insert collision."));            
+  })
+
+  it('should throw error if duplicating an entry', function() {
+    var some_string = "jonathan"; //key evaluates to 5
+    hashTable.insert(some_string);
+    expect(function(){ 
+      debugger;    
+      hashTable.insert("jonathan"); // key evaluates to 5
+    }).toThrow(new Error("Duplicate entry attempted."));            
+  })
+
 });
--- src/hashTable.js
+++ src/hashTable.js
@@ -1,5 +1,5 @@
 var HashTable = function(){
-  this._limit = 8;
+  this._limit = 2;
 
   // Use a limited array to store inserted elements.
   // It'll keep you from using too much space. Usage:
@@ -10,15 +10,41 @@ var HashTable = function(){
   this._storage = makeLimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(){
+HashTable.prototype.insert = function(aString){
+  var key = this.fetchKey(aString);
+  if(this._storage[key]) {
+    if(this._storage[key] === aString) {
+      throw new Error("Duplicate entry attempted.");
+    } else {
+      console.log("running collision resolution");
+      throw new Error("Insert collision.");
+    }
+  } else {
+    this._storage[key] = aString;    
+  }
 };
 
-HashTable.prototype.retrieve = function(){
+HashTable.prototype.retrieve = function(aString){
+  var key = this.fetchKey(aString);
+  if (this._storage[key] === undefined) {
+    throw new Error("Cannot retrieve string.");
+  } else {
+    return this._storage[key];
+  };
 };
 
-HashTable.prototype.remove = function(){
-};
+HashTable.prototype.remove = function(aString){
+  var key = this.fetchKey(aString);
+  if (this._storage[key] === undefined) {
+    throw new Error("String does not exist.");
+  } else {
+    delete this._storage[key];
+  };
+}
 
+HashTable.prototype.fetchKey = function(aString) {
+  return getIndexBelowMaxForKey(aString, this._limit);
+};
 // NOTE: For this code to work, you will NEED the code from hashTableHelpers.js
 // Start by loading those files up and playing with the functions it provides.
 // You don't need to understand how they work, only their interface is important to you

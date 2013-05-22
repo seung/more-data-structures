2c858dbd031120ec4541a9649d553a904dea50da
seung <selim50@gmail.com>
2013-05-22 13:36:29 -0600

    Minor cleanup

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -3,50 +3,36 @@ var makeBinarySearchTree = function(val){
   bst.value = val;
   bst.leftNode;
   bst.rightNode;
-
   return bst;
 };
 
 var bstMethods = {
   insert: function(val) {
     var newNode = makeBinarySearchTree(val);
-
     var addNode = function(obj, newNode) {
-      console.log("this value: " + obj.value + ", new node: " + newNode.value);
-      // debugger;
       if (obj.value > newNode.value) {
         obj.leftNode = newNode;
       } else if (obj.value < newNode.value){
         obj.rightNode = newNode;
       } else {
-        debugger;
         throw new Error("Value already is in tree.");
-      }
-    }
-
+      };
+    };
     var myFunc = function() {
       addNode(this,newNode);
     };
-
     this.depthFirstLog(myFunc);
-  },
-
-
-  contains: function() {},
-}
+  }
+};
 
 bstMethods.depthFirstLog = function(func) {
   var traverse = function(obj, func) {
     if (!obj.leftNode && !obj.rightNode) {
       func(obj);
     } else {
-      if(obj.leftNode) {
-        traverse(obj.leftNode, func);
-      }
-      if(obj.rightNode) {
-        traverse(obj.rightNode, func);
-      }
-    }
+      (obj.leftNode) && traverse(obj.leftNode, func);
+      (obj.rightNode) && traverse(obj.rightNode, func);
+    };
   };
   traverse(this, func);
 };
